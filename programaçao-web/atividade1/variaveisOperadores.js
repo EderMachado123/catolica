7e110f303ff6d7c449881f5819eb5219bf869743
eder machado viana de olievira <edermachadovianadeolievira@gmail.com>
2025-02-28 11:00:47 -0300

    atv 1

---
+++ programaçao-web/atividade1/variaveisOperadores.js
@@ -0,0 +1,33 @@
+var x = 1;
+var y = 2;
+soma = x + y;
+
+console.log(`o resultado da soma entre ${x} e ${y} é ${soma}`);
+
+// ted
+
+
+var x = 2;
+var y = 1;
+subtração = x - y;
+
+console.log(`o resultado da subtração entre ${x} e ${y} é ${subtração}`);
+
+
+
+
+var x = 4;
+var y = 2;
+multiplicação = x * y;
+
+console.log(`o resultado da multiplicação entre ${x} e ${y} é ${multiplicação}`);
+
+
+
+
+
+var x = 4;
+var y = 2;
+divisão = x / y;
+
+console.log(`o resultado da divisão entre ${x} e ${y} é ${divisão}`);
